ec196a5fda7078bac7519047c30612fab4b71205
Roy Stanfield <roystanfield@gmail.com>
2013-06-23 18:07:24 -0400

    Add association for User and Pins

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,6 @@ class User < ActiveRecord::Base
   attr_accessible :email, :password, :password_confirmation,
                   :remember_me, :name
   # attr_accessible :title, :body
+
+  has_many :pins
 end
