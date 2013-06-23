8fedac49115af2543e6a10dda7b3d817a12a1a95
Roy Stanfield <roystanfield@gmail.com>
2013-06-23 18:45:20 -0400

    Corrected gitignore and added support for PNGs

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -5,7 +5,7 @@ class Pin < ActiveRecord::Base
   validates :description, presence: true
   validates :user_id, presence: true
   validates_attachment :image, presence: true,
-                              content_type: { content_type: [ 'image/jpeg', 'image/jpg', 'image/gif' ]},
+                              content_type: { content_type: [ 'image/jpeg', 'image/jpg', 'image/gif', 'image/png' ]},
                               size: { less_than: 5.megabytes }
 
   belongs_to :user
